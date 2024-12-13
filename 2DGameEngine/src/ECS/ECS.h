7e17cb6db018b2c3aa747647f1e45687874d37ac
Nguyên Phạm <motsach1732002@gmail.com>
2024-12-13 13:00:57 +0700

    add/remove entities from systems, pool class, registry

--- 2DGameEngine/src/ECS/ECS.h
+++ 2DGameEngine/src/ECS/ECS.h
@@ -3,6 +3,8 @@
 
 #include <bitset>
 #include <vector>
+#include <unordered_map>
+#include <typeindex>
 
 const unsigned int MAX_COMPONENTS = 32;
 
@@ -31,7 +33,14 @@ class Entity {
 		int id;
 	public:
 		Entity(int id) : id(id) {};
+		Entity(const Entity& entity) = default;
 		int GetId() const;
+
+		Entity& operator =(const Entity& other) = default;
+		bool operator ==(const Entity& other) const { return id == other.id; }
+		bool operator !=(const Entity& other) const { return id != other.id; }
+		bool operator >(const Entity& other) const { return id > other.id; }
+		bool operator <(const Entity& other) const { return id < other.id; }
 };
 
 /// <summary>
@@ -50,15 +59,100 @@ class System {
 		void AddEntityToSystem(Entity entity);
 		void RemoveEntityFromSystem(Entity entity);
 		std::vector<Entity> GetSystemEntities() const;
-		Signature& GetComponentSignature() const;
+		const Signature& GetComponentSignature() const;
 
 		// Define the component type that entities must have to be considered by the system
 		template <typename TComponent> void RequireComponent();
 };
 
+/// <summary>
+/// Pool
+/// A pool is just a vector (continous data) of objects of type T
+/// </summary>
+
+class IPool {
+	public:
+		virtual ~IPool() {}
+};
+
+template <typename T>
+class Pool:public IPool {
+	private:
+		std::vector<T> data;
+
+	public:
+		Pool(int size = 100) {
+			data.resize(size);
+		}
+		 virtual Pool() = default;
+
+		bool isEmpty() const {
+			return data.empty();
+		}
+
+		int GetSize() const {
+			return data.size();
+		}
+
+		void Resize(int n) {
+			data.resize(n);
+		}
+
+		void Clear() {
+			data.clear();
+		}
+
+		void Add(T object) {
+			data.push_back(object);
+		}
+
+		void Set(int index, T object) {
+			data[index] = object;
+		}
+
+		T& Get(int index) {
+			return static_cast<T&>(data[index]);
+		}
+
+		T& operator [](unsigned int index) {
+			return data[index];
+		}
+};
+
+/// <summary>
+/// Registry 
+/// The Registry manages the creation and destruction of entities
+/// adding systems and adding components to entities
+/// </summary>
 class Registry {
+	private:
+		int numEntities = 0;
+
+		// Vector of component pools, each pool contains all the data for a certain component type
+		// Vector index = component type id
+		// Pool index == entity id
+		std::vector<IPool*> componentPools;
+
+		// Vector of component signatures per entity, saying which component is turned "on" for a given entity
+		// [Vector index = entity id]
+		std::vector<Signature> entityComponentSignatures;
+
+		std::unordered_map<std::type_index, System*> systems;
+
+	public:
+		Registry() = default;
+
+		// TODO:
+		// 
+		// createEntity
+		// 
+		// addComponent, removeComponent
+
+		// GetComponent
+
+		// add system()
 
 };
 
 
-#endif // ! ECS_H
+#endif 
